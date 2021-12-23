90cc1eff909b6941b37a3b17a2fb2f0c52103bc2
Konstantin Läufer <klaeufer@gmail.com>
2021-12-23 13:21:36 -0600

    added option to skip leading rows
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- vrn7500tools/chirp2cg.py
+++ vrn7500tools/chirp2cg.py
@@ -9,7 +9,7 @@ import sys
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--name", help="channel group name", nargs='?', type=str, default="Unnamed Channel Group")
-    parser.add_argument("-s", "--skip", help="number of rows to skip", nargs='?', type=int, default=0)
+    parser.add_argument("-s", "--skip", help="number of leading rows to skip", nargs='?', type=int, default=0)
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG)
