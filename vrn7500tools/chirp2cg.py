a84252d7a383c06348c60e0fb56037d7fd225cb8
Konstantin Läufer <klaeufer@gmail.com>
2021-12-23 19:56:41 -0600

    added brief description to ArgParse
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- vrn7500tools/chirp2cg.py
+++ vrn7500tools/chirp2cg.py
@@ -3,11 +3,12 @@
 from . import helpers
 
 import argparse
+import itertools
 import logging
 import sys
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="converts CHIRP csv from RepeaterBook to JSON channel group for Vero N-7500/Retevis RT99")
     parser.add_argument("-n", "--name", help="channel group name", nargs='?', type=str, default="Unnamed Channel Group")
     parser.add_argument("-s", "--skip", help="number of leading rows to skip", nargs='?', type=int, default=0)
     args = parser.parse_args()
