b76a4431a3989034d26f9b53c7ddda3005a2200b
Konstantin Läufer <klaeufer@gmail.com>
2021-12-23 19:57:16 -0600

    added shell script to iterate in create of 14
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

---
+++ iterate.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+MAX_CHANNELS=14
+IFS=$'\n'
+
+for (( i=1; i <= $1; i++ ))
+do
+    for f in *.csv
+    do              
+        bname=${f%.csv}
+        echo generating channel group $bname $i
+        chirp2cg -s $(( (i - 1) * $MAX_CHANNELS )) -n "$bname $i" > "${bname}-$i.json" < $f
+    done
+done
